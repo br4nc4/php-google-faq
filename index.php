2a0ef68a212b601b2681d91de1b5b398709c216a
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-12 00:04:14 +0200

    sottotitolo problema di stampa

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $db = [
             "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
             "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.",
         ],
-        "sottotilo" => ""
+        "sottotitolo" => ""
     ],
     [
         "titolo" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
@@ -19,7 +19,7 @@ $db = [
             "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online</a>.",
             "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
         ],
-        "sottotilo" => ""
+        "sottotitolo" => ""
     ],
     [
         "titolo" => "Perché il mio account è associato a un paese?",
@@ -38,14 +38,14 @@ $db = [
         "paragrafo" => [
             "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='#'>fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>."
         ],
-        "sottotilo" => ""
+        "sottotitolo" => ""
     ],
     [
         "titolo" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
         "paragrafo" => [
             "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='#'>URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
         ],
-        "sottotilo" => ""
+        "sottotitolo" => ""
     ]
 ];
 
@@ -110,11 +110,14 @@ $db = [
         <hr>
     </header>
     
-    <main class="mt-5">
+    <main class="mt-4">
         <div class="container">
             <?php 
             foreach ($db as $i => $section) {
                 echo "<h4 class='mb-4'>" . $section["titolo"] . "</h4>";
+                if($section["sottotitolo"] !== " "){
+                    echo "<h6>". $section["sottotitolo"] . "</h6>";
+                }
                 foreach($section["paragrafo"] as $risposta) {
                     echo "<p>" . $risposta . "</p>";
                 }
