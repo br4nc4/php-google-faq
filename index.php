4837ed61eb0208b7fadd31bcb4107441a522307d
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-11 16:42:18 +0200

    h4 printed (testing)

--- index.php
+++ index.php
@@ -46,7 +46,13 @@ $db = [
             "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='#'>URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
         ]
     ]
-]
+];
+
+/* var_dump($db); */
+/* foreach($db as $i => $paragrafo){
+    $paragrafo
+} */
+
 ?>
 
 <!DOCTYPE html>
@@ -99,7 +105,13 @@ $db = [
     
     <main>
         <div class="container">
-
+            <?php 
+            for($i=0; $i<count($db); $i++) { 
+                $titolo = $db[$i];
+                echo "<h4 class='mb-4'>" . $titolo["titolo"] . "</h4>";
+            }
+            ?>
+            
         </div>
     </main>
 </body>
